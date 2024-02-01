a4ce0757546465a307d660510d004b405e4f5b6b
Top-5 <top-5>
2024-02-01 00:02:31 -0800

    Build script for Windows

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -19,21 +19,16 @@ ext_modules = [
 setup(name='pycocotools',
       packages=['pycocotools'],
       package_dir = {'pycocotools': 'pycocotools'},
-      version='2.0',
+      setup_requires=[
+        'setuptools>=18.0',
+        'cython>=3.0.8'
+      ],
+      install_requires=[
+        'setuptools>=18.0',
+        'cython>=3.0.8',
+        'matplotlib>=3.8.2'
+      ],
+      version='2.0.7',
       ext_modules=
           cythonize(ext_modules)
       )
-
-# setup(
-#    name='pycocotools',
-#    packages=['pycocotools'],
-#    package_dir = {'pycocotools': 'pycocotools'},
-#    install_requires=[
-#        'setuptools>=18.0',
-#        'cython>=0.27.3',
-#        'matplotlib>=2.1.0'
-#    ],
-#    version='2.0',
-#    ext_modules= ext_modules
-#
-# )
